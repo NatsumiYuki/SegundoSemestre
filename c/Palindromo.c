d2592133a19ffd40443f4a69dfbd654a43c2e47c
Natsumi Yuki <natsumiyuuki.a@gmail.com>
2024-08-14 01:51:53 -0300

    Palindromo

---
+++ c/Palindromo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    char palavra[50];
+    int i = 0, tamanho, igual = 0;
+
+    printf ("Insira a palavra:\n");
+    scanf ("%s", palavra);
+
+    tamanho = strlen(palavra);
+    // 0
+    tamanho--;
+
+    while(i <= tamanho){
+        if (palavra[i] != palavra[tamanho]){
+            igual++;
+        }
+        i++;
+        tamanho--;
+    }
+
+    if (igual == 0){
+        printf ("P");
+    } else {
+        printf ("NP");
+    }
+    return 0;
+}
